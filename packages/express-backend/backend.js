3010bb1a3e07f04ba787566b7689b4fdd9d1e1c2
Jacob Lilien <jlilien@calpoly.edu>
2024-10-03 22:34:26 -0700

    Added POST method, delete, and put

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -33,14 +33,34 @@ const users = {
     ]
   };
 
-  const findUserByName = (name) => {
+  const filterUserByName = (name) => {
     return users["users_list"].filter(
       (user) => user["name"] === name
     );
   };
 
+  const filterUserByID = (id) => {
+    return users["users_list"].filter(
+      (user) => user["id"] === id
+    );
+  };
+
   const findUserByID = (id) =>
     users["users_list"].find((user) => user["id"] === id);
+
+  const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+  };
+
+  const deleteUser = (id) => {
+    const index = users["users_list"].findIndex(user => user["id"] === id)
+    if (index !== -1)
+    {
+      users["users_list"].splice(index, 1)
+    }
+    return users["users_list"]
+  };
 
 app.use(express.json());
 
@@ -51,12 +71,24 @@ app.get("/", (req, res) => {
 app.get("/users", (req, res) => {
   const name = req.query.name;
   if (name != undefined) {
-    let result = findUserByName(name);
+    let result = filterUserByName(name);
     result = { users_list: result };
     res.send(result);
   } else {
     res.send(users);
   }
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
+app.delete("/users", (req, res) => {
+  const userToDelete = req.body["id"];
+  deleteUser(userToDelete);
+  res.send(userToDelete);
 });
 
 app.get("/users/:id", (req, res) => {
@@ -66,6 +98,19 @@ app.get("/users/:id", (req, res) => {
     res.status(404).send("Resource not found.");
   } else {
     res.send(result);
+  }
+});
+
+app.put("/users", (req, res) => {
+  const name = req.body["name"]
+  const id = req.body["id"]
+  let resultname = filterUserByName(name);
+  let resultid = filterUserByID(id)
+  if (resultname["name"] == resultid["name"] && resultname["id"] == resultid["id"]){
+    res.send(resultname)
+  }
+  else {
+    res.send("No Match")
   }
 });
 
